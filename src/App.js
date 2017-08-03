dbc11897c58e124eaea3cf9cd637c660d670bf8e
Marty Penner <penner.marty@gmail.com>
2017-08-03 14:41:28 -0400

    feat: Make book listings dynamic from API

--- src/App.js
+++ src/App.js
@@ -3,19 +3,34 @@ import './App.css';
 import React from 'react';
 import { Route } from 'react-router-dom';
 
+import * as BooksAPI from './BooksAPI';
 import BookList from './components/BookList';
 import SearchBooks from './components/SearchBooks';
 import routes from './constants/routes';
 
-// import * as BooksAPI from './BooksAPI';
+if (process.env.NODE_ENV === 'development') {
+  BooksAPI.getAll().then(books =>
+    console.info('initial books from API', books)
+  );
+}
 
 class BooksApp extends React.Component {
-  state = {};
+  state = { books: [] };
+
+  componentDidMount() {
+    BooksAPI.getAll().then(books => this.setState({ books }));
+  }
 
   render() {
+    const { books } = this.state;
+
     return (
       <div className="app">
-        <Route exact path={routes.ROOT} component={BookList} />
+        <Route
+          exact
+          path={routes.ROOT}
+          render={() => <BookList books={books} />}
+        />
 
         <Route path="/search" component={SearchBooks} />
       </div>
